ee2317f29e04f45455e8019b525bbf27d01d72a4
unknown <smetchappy@gmail.com>
2026-06-15 16:45:39 +0200

    Create dedicated About Us page explaining marketplace platform role and full-stack tracking capabilities

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ app = Flask(
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
